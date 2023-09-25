a4c965daa603f7045fc0876f58bc17d357e36ecc
kongzh95 <kongzh95@hotmail.com>
2023-09-25 22:06:41 +0000

    add data and model path.

--- src/config/infer_config.py
+++ src/config/infer_config.py
@@ -1,34 +1,39 @@
 import numpy as np
 
 ## Infer options ##
-DATAFILE_PATH = ''
+DATAFILE_PATH = "data/pendulum_ctr_grf_N_100_T10.npy"
 PLOT_TRAJS = True
 PLOT_IDXS = [0]
-TRAINED_MODEL_PATH = ''
-FIGURE_PATH = 'figures/'
+TRAINED_MODEL_PATH = "runs:/420420f074ae4d78928d3e85d131a4e0/best_model_epoch_54"
+FIGURE_PATH = "figures/"
 VERBOSE = True
-LOSS_FUNCTION='MSE'
+LOSS_FUNCTION = "MSE"
 
 ## Data options ##
-SEARCH_LEN = 10
-SEARCH_NUM = 5
-SEARCH_RANDOM = False
 OFFSET = 0
-SCALE_MODE = ''
+T_MAX = 10
+STATE_COMPONENT = 0
+SEARCH_LEN = 10
+SEARCH_NUM = 10
+SEARCH_RANDOM = False
+SCALE_MODE = ""
+
 
 def get_config():
     config = {
-        'datafile_path': DATAFILE_PATH,
-        'search_len': SEARCH_LEN,
-        'search_num': SEARCH_NUM,
-        'search_random': SEARCH_RANDOM,
-        'offset': OFFSET,
-        'verbose': VERBOSE,
-        'scale_mode': SCALE_MODE,
-        'loss_function': LOSS_FUNCTION,
-        'plot_trajs': PLOT_TRAJS,
-        'plot_idxs': PLOT_IDXS,
-        'trained_model_path': TRAINED_MODEL_PATH,
-        'figure_path': FIGURE_PATH
-        }
+        "datafile_path": DATAFILE_PATH,
+        "state_component": STATE_COMPONENT,
+        "search_len": SEARCH_LEN,
+        "search_num": SEARCH_NUM,
+        "search_random": SEARCH_RANDOM,
+        "offset": OFFSET,
+        "t_max": T_MAX,
+        "verbose": VERBOSE,
+        "scale_mode": SCALE_MODE,
+        "loss_function": LOSS_FUNCTION,
+        "plot_trajs": PLOT_TRAJS,
+        "plot_idxs": PLOT_IDXS,
+        "trained_model_path": TRAINED_MODEL_PATH,
+        "figure_path": FIGURE_PATH,
+    }
     return config
