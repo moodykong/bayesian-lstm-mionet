2a27b31e7cd329345c7b8b447d69aa0c4ad3ae88
kongzh95 <kongzh95@hotmail.com>
2023-09-25 22:05:21 +0000

    add infer.py using mlflow.

---
+++ src/infer.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import numpy as np
+import torch
+import mlflow
+from utils import torch_utils
+from optim.supervisor import execute_test
+from utils.data_utils import (
+    Dataset_Torch,
+    Dataset_Stat,
+    prepare_local_predict_dataset,
+    scale_and_to_tensor,
+    split_dataset,
+)
+from config import infer_config
+from utils.args_parser import add_infer_args, args_to_config
+import argparse, os
+
+# Set current working directory
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+
+def run(config):
+    ###################################
+    # Step 1: initialize the gpu
+    ###################################
+    torch_utils.init_gpu(use_gpu=False, verbose=config["verbose"])
+
+    ###################################
+    # Step 2: set the seed
+    ###################################
+    seed = 9999
+    np.random.seed(seed=seed)
+    torch.manual_seed(seed)
+
+    ###################################
+    # Step 3: collect the dataset
+    ###################################
+
+    test_dataset = config["datafile_path"]
+    test_dataset = np.load(test_dataset, allow_pickle=True).item()  # load the .npy file
+
+    ###################################
+    # Step 4: prepare training and test datasets
+    ###################################
+
+    _, test_data = split_dataset(test_dataset, test_size=1, verbose=config["verbose"])
+
+    input_masked, x_n, x_next, t_params = prepare_local_predict_dataset(
+        data=test_data,
+        state_component=config["state_component"],
+        search_len=config["search_len"],
+        search_num=config["search_num"],
+        search_random=config["search_random"],
+        offset=config["offset"],
+        t_max=config["t_max"],
+        verbose=config["verbose"],
+    )
+    test_data_stat = Dataset_Stat(input_masked, x_n, x_next, t_params)
+    state_feature_num = x_n.shape[-1]
+
+    ###################################
+    # Step 5: update dataset statistics
+    ###################################
+    test_data_stat.update_statistics()
+
+    ###################################
+    # Step 6: scale and move numpy data to tensor
+    ###################################
+
+    input_masked, x_n, x_next, t_params = scale_and_to_tensor(
+        test_data_stat, scale_mode=config["scale_mode"], device=torch_utils.device
+    )
+    test_data_torch = Dataset_Torch(input_masked, x_n, x_next, t_params)
+
+    ###################################
+    # Step 7: load the model using mlflow
+    ###################################
+    model = mlflow.pytorch.load_model(config["trained_model_path"])
+
+    if config["verbose"]:
+        print(model)
+
+    execute_test(config=config, model=model, dataset=test_data_torch)
+
+
+def main():
+    # Load the configurations
+    config = dict()
+    config.update(infer_config.get_config())
+    # Add the arguments
+    parser = argparse.ArgumentParser()
+    parser = add_infer_args(parser)
+    # Parse the arguments
+    args_config = args_to_config(parser)
+    # Update the configurations
+    config.update(args_config)
+    # Run the program
+    run(config)
+
+
+if __name__ == "__main__":
+    main()
