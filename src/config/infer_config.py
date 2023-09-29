3ccce03efa6965b9766e92497eef94c60512540a
kongzh95 <kongzh95@hotmail.com>
2023-09-29 19:25:09 +0000

    add device to config. add more trained file paths.

--- src/config/infer_config.py
+++ src/config/infer_config.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 ## Infer options ##
-DATAFILE_PATH = "data/pendulum_ctr_grf_N_100_h001_T10.npy"
+DATAFILE_PATH = "data/Ausgrid_cust_51-60.npy"
 PLOT_TRAJS = True
 PLOT_IDXS = [0]
 # TRAINED_MODEL_PATH = (
@@ -13,22 +13,27 @@ PLOT_IDXS = [0]
 # TRAINED_MODEL_PATH = (
 #    "runs:/09ea52ba93d54fc4b4efb715709e1ff7/best_model_epoch_381"  # lorentz h=0.01
 # )
-TRAINED_MODEL_PATH = (
-    "runs:/5e1fec5193a3437bb041dea1aa00fb80/best_model_epoch_280"  # pendulum h=0.2
-)
+# TRAINED_MODEL_PATH = (
+#    "runs:/5e1fec5193a3437bb041dea1aa00fb80/best_model_epoch_280"  # pendulum h=0.2
+# )
+# TRAINED_MODEL_PATH = "runs:/dc2b156f846347289bcb1be4fe054f85/best_model_epoch_100"  # Ausgrid customer 1-50, h=0.25
+TRAINED_MODEL_PATH = "runs:/f59b5c9580d24ef08aa32bd8cc77a9cb/best_model_epoch_140"  # Ausgrid customer 1-50, h=0.5
 
 FIGURE_PATH = "figures/"
 VERBOSE = True
 
 ## Data options ##
 OFFSET = 0
-T_MAX = 10
+T_MAX = None
 STATE_COMPONENT = 0
-SEARCH_LEN = 20
-SEARCH_NUM = 200
+SEARCH_LEN = 10
+SEARCH_NUM = 50
 BATCH_SIZE = 100
 SEARCH_RANDOM = False
 SCALE_MODE = ""
+
+## Device options ##
+DEVICE = 1  # int,"parallel","cpu"
 
 
 def get_config():
@@ -47,5 +52,6 @@ def get_config():
         "plot_idxs": PLOT_IDXS,
         "trained_model_path": TRAINED_MODEL_PATH,
         "figure_path": FIGURE_PATH,
+        "device": DEVICE,
     }
     return config
